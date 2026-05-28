c228ce838d2e441e5e164c54b42787f513205679
Huzaifa Moin <huzaifamoin784@gmail.com>
2026-05-28 21:00:49 +0500

    url updated in cors

--- server/server.ts
+++ server/server.ts
@@ -25,7 +25,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://ai-thumbnail-zzfe.vercel.app/'],
     credentials: true,
 }));
 
